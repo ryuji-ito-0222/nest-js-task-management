dbcbefda10202940ae2b8508b34929dec841935f
RyujiIto <ryuji0222ss@gmail.com>
2021-03-21 05:56:55 +0900

    ➕

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  console.log(serverConfig.port);
+  app.enableCors();
+  const serverConfig = config.get('server');
 
   const port = process.env.PORT || serverConfig.port || 3001;
   await app.listen(port);
